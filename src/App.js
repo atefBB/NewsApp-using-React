aab1ce42313b2c6b6440008305cdb61b0ae6457e
Atef Ben Ali <atef.bettaib@gmail.com>
2021-07-24 17:26:35 +0100

    chore: clean-up

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import React from "react";
-import "./App.css";
+
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
 import SingleNews from "./Components/NewsList/SingleNews/SingleNews";
-import Layout from "./Components/Menubar/Layout/Layout"
+import Layout from "./Components/Menubar/Layout/Layout";
 import Footer from "./Components/Footer/Footer";
 import NewsList2 from "./Components/NewsList2/NewsList2";
-import Newslist from "./Components/NewsList/Newslist";
-function App() {
+
+import "./App.css";
+
+export default function App() {
   return (
     <div className="App">
-      
-      <BrowserRouter basename={'NewsApp-using-React#'}>
-      <Layout/>
+      <BrowserRouter basename={"NewsApp-using-React#"}>
+        <Layout />
         <Switch>
-        <Route exact path="/NewsApp-using-React/" render={() => (<Redirect to="#/Home" />)} />
+          <Route
+            exact
+            path="/NewsApp-using-React/"
+            render={() => <Redirect to="#/Home" />}
+          />
           <Route path="/:category" exact component={NewsList2}></Route>
           <Route path="/:category/:id" component={SingleNews}></Route>
-        
-        
-          <Route exact path="/" render={() => (<Redirect to="/Home" />)} /> 
+
+          <Route exact path="/" render={() => <Redirect to="/Home" />} />
         </Switch>
-       
       </BrowserRouter>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
-
-export default App;
